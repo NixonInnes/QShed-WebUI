263dd77e61a66637de91929f2304eac8b7ab7a60
NixonInnes <nixoninnes@echonet.io>
2022-03-09 22:40:16 +0000

    Fixes & added form to add new schedule

--- app/blueprints/scheduler/forms.py
+++ app/blueprints/scheduler/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, TextAreaField, SubmitField, SelectField, IntegerField
 from wtforms.validators import DataRequired, Length
 
 
@@ -8,3 +8,13 @@ class ExampleForm(FlaskForm):
         "Text: ", default="A default string", validators=[DataRequired(), Length(5)]
     )
     submit = SubmitField()
+
+
+class NewScheduleForm(FlaskForm):
+    interval = IntegerField("Interval")
+    url = StringField("URL")
+    method = SelectField("Method", choices=("get", "post"))
+    params = TextAreaField("URL Parameters")
+    data = TextAreaField("Request Data")
+    headers = TextAreaField("Request Headers")
+    submit = SubmitField("Submit")
--- app/blueprints/scheduler/routes.py
+++ app/blueprints/scheduler/routes.py
@@ -1,8 +1,11 @@
-import yaml
-from flask import render_template, redirect, request, url_for, current_app
+import json
+from flask import render_template, redirect, url_for
+
+from bootlets.boots import QuickForm
 
 from ... import client
 from . import sched_bp
+from .forms import NewScheduleForm
 
 
 @sched_bp.route("/", methods=["GET"])
@@ -10,7 +13,24 @@ def index():
     return render_template("index.html")
 
 @sched_bp.route("/list", methods=["GET"])
-def list_schedulers():
-    schedulers = client.scheduler.list()
-    schedulers = [yaml.dump(scheduler) for scheduler in schedulers]
-    return render_template("scheduler/list.html", schedulers=schedulers)
+def list_schedules():
+    schedules = client.scheduler.list()
+    return render_template("scheduler/list.html", schedules=schedules)
+
+
+@sched_bp.route("/add", methods=["GET", "POST"])
+def add_schedule():
+    form = NewScheduleForm()
+
+    if form.validate_on_submit():
+        client.scheduler.add(
+            interval=form.interval.data,
+            url=form.url.data,
+            method=form.method.data,
+            params=json.loads(form.params.data) if form.params.data else {},
+            data=json.loads(form.data.data) if form.data.data else {},
+            headers=json.loads(form.headers.data) if form.headers.data else {}
+        )
+        return redirect(url_for("scheduler.list_schedules"))
+
+    return render_template("form.html", title="New Schedule", form=QuickForm(form))
